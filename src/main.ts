767ad22cf556f966c5006b4355a4013ef9aabd59
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2024-04-24 17:17:28 +0100

    added swagger and user module

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   app.enableCors({
     origin: '*',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Backend Implementation')
+    .setDescription('A general backend API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
